94a944a6fb1cd5cd35c9dad8ec00dbb77295dbfc
James-S-B <jamessbenerofe23@gmail.com>
2021-12-01 13:36:04 -0800

    update verification

--- complete-project-08/src/components/Users/AddUser.js
+++ complete-project-08/src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
+
 import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
