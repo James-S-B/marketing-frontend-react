1d8bb393c14d53f812e8b7858c144fb6ec2b590b
sbenerofe <sbenerofe@gmail.com>
2021-12-01 13:21:34 -0800

    setup entry validation

---
+++ complete-project-08/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import UsersList from "./components/Users/UsersList";
+
+import AddUser from "./components/Users/AddUser";
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      console.log("new", usersList);
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
