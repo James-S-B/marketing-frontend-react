31b47b08e5d337e841c5e77d253a73283badfb05
James-S-B <jamessbenerofe23@gmail.com>
2022-02-02 10:27:44 -0800

    cart

--- code/01-starting-project/src/components/Layout/Header.js
+++ code/01-starting-project/src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     <react.Fragment>
       <header className={classes.header}>
         <h1>RactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of food" />
